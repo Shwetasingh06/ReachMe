7194773451baa9edfedfcab401ad929582a9b2f5
Shwetasingh06 <40825340+Shwetasingh06@users.noreply.github.com>
2020-07-30 11:14:47 +0530

    shweta

---
+++ app/src/main/java/com/example/reachme/auth/RegisterFragment.java
@@ -0,0 +1,29 @@
+package com.example.reachme.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.reachme.R;
+
+
+public class RegisterFragment extends Fragment {
+
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
