be1be3c62854f0a42f360575d0789a8ea76be6fb
KUS MAURYA <utsav.singh0023@gmail.com>
2020-07-31 13:14:23 +0530

    swapping is improved and some correction are made in button of login and register

---
+++ app/src/main/java/com/example/reachme/home/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.reachme.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.reachme.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
